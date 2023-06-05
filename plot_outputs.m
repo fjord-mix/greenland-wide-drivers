6fa6144b3c10b8ca9e4781783b0d49df05bf10a8
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-06-05 14:45:32 +0100

    now accepts a figure handle, and returns its own handle

--- plot_outputs.m
+++ plot_outputs.m
@@ -1,4 +1,4 @@
-function plot_outputs(fjord_model1,fjord_model2)
+function hf = plot_outputs(hf,fjord_model1,fjord_model2)
 
 model_runtime1 = fjord_model1.s.t(1:size(fjord_model1.s.H,2));
 runtime_axis = fjord_model1.m.time_axis;
@@ -10,7 +10,11 @@ end
 
 
 m=3; n=1;
-figure('Position',[20 20 800 600],'Name',fjord_model1.m.name); 
+if isempty(hf)
+    hf=figure('Position',[20 20 800 600],'Name',fjord_model1.m.name); 
+else
+    set(gcf,hf);
+end
 subplot(m,n,1); plot(taxis1,fjord_model1.s.H,'linewidth',1.5); hold on;
 ylabel('Thickness (m)'); legend('1','2','3','4','Location','east');
 subplot(m,n,2); plot(taxis1,fjord_model1.s.T,'linewidth',1.5); hold on;
@@ -18,7 +22,7 @@ ylabel('Temperature (^oC)');
 subplot(m,n,3); plot(taxis1,fjord_model1.s.S,'linewidth',1.5); hold on;
 ylabel('Salinity'); xlabel('Model time (days)')
 
-if nargin > 1
+if nargin > 2
     model_runtime2 = fjord_model2.s.t(1:size(fjord_model1.s.H,2));
     runtime_axis = fjord_model2.m.time_axis;
     t0 = convertTo(runtime_axis(1),'datenum');
