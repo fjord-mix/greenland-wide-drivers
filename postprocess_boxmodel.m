63f0d94d2660025510e9b5509b55c689a7be9aa7
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-06-13 10:50:40 +0100

    function to output useful quantities from the model

---
+++ postprocess_boxmodel.m
@@ -0,0 +1,44 @@
+function outputs = postprocess_boxmodel(fjord)
+
+    % output variables of interest
+    z_profiles=fjord.f.zs;
+    temp_profiles=NaN(size(fjord.f.Ts));
+    salt_profiles=NaN(size(fjord.f.Ss));
+    t_export=NaN(size(fjord.t));
+    s_export=NaN(size(fjord.t));
+    v_export=NaN(size(fjord.t));
+    z_bnds_export=NaN([2,length(fjord.t)]);
+
+    % iterates over all time steps
+    for i=1:length(fjord.s.t) 
+
+        % copies profile values for all depths related to that box
+        box_depths=-[0, cumsum(fjord.s.H(:,i)')];
+        for k=1:length(box_depths)-1
+            inds_box=z_profiles < box_depths(k) & z_profiles > box_depths(k+1);
+            temp_profiles(inds_box,i)=fjord.s.T(k,i);
+            salt_profiles(inds_box,i)=fjord.s.S(k,i);
+        end
+        
+        i_outflow = find(fjord.s.QVs(:,i) > 0); % finds where flow is going towards the shelf
+        [~,i_export] = max(fjord.s.QVs(i_outflow,i)); % finds the strongest flow
+
+        % if something is being exported
+        if ~isempty(i_export) 
+            z_bnds_export(:,i)=box_depths(i_export:i_export+1); % gets the depth interval
+            t_export(i)=fjord.s.T(i_export,i);                  % copies over the associated heat,
+            s_export(i)=fjord.s.S(i_export,i);                  % salt,
+            v_export(i)=fjord.s.V(i_export,i);                  % and volume fluxes
+        end
+
+    end
+
+    % copies all into output structure
+    outputs.Tf = temp_profiles;
+    outputs.Sf = salt_profiles;
+    outputs.Ze = smoothdata(z_bnds_export,2);
+    outputs.Te = smoothdata(t_export);
+    outputs.Se = smoothdata(s_export);
+    outputs.Ve = smoothdata(v_export);
+
+end
