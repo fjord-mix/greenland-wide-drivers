ee76ccb431ae954e7b746379e3e4266eb6754d1f
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-07-11 12:52:56 +0100

    code tidy up to comply with current data-collation and box-model repo changes

--- exps_icebergs.m
+++ exps_icebergs.m
@@ -1,36 +1,42 @@
 %% Sensitivity to iceberg melt efficiency, i.e., varying a single parametre
+id=1;
+[p_def,~] = get_model_default_parameters();
+if id==2 || id==4 % we remove the sill for Sermilik and Ilulissat
+    p_def.sill=0;
+end
 
-% Defining the parametre space
-M0_space = [2e-8,2e-10,2e-9,2e-7,2e-6]; % default: 2e-8
+M0_space = [2e-8,2e-10,2e-9,2e-7,2e-6]; % Defining the parametre space (default M0: 2e-8)
+
+exp_out_path=[outs_path,'/sens_icebergs/'];
+mkdir(exp_out_path)
 
 % Iterating over a loop performing one run for each combination
 for i_param=1:length(M0_space)
     name_exp = sprintf('%s_M0%0.1e',fjord_keys{id},M0_space(i_param));    
     fprintf('Running model for %s... ',name_exp)
-    fjord_run = prepare_boxmodel_input(datasets,fjords_processed(fjord_ids(id)),fjord_ids(id));
+    fjord_run = prepare_boxmodel_input(datasets,fjords_processed(fjord_ids(id)),fjord_ids(id),p_def);
     fjord_run.p.M0=M0_space(i_param);
     tic
-    fjord_run.s = boxmodel(fjord_run.p,fjord_run.f,fjord_run.a,fjord_run.t); % runs and gets the results    
+    [fjord_run.s,fjord_run.f] = boxmodel(fjord_run.p,fjord_run.f,fjord_run.a,fjord_run.t); % runs and gets the results    
     if ~fjord_run.s.status, disp('Done.'); end
     toc    
     fjord_run.o = postprocess_boxmodel(fjord_run);
     fjord_run.m.name = name_exp;   
-    save([outs_path,name_exp,'_MPhr.mat'],'fjord_run','-v7.3') % v7.3 allows files > 2GB; MPhr stands for the ocean forcing used    
+    save([exp_out_path,name_exp,'.mat'],'fjord_run','-v7.3') % v7.3 allows files > 2GB; MPhr stands for the ocean forcing used    
 end
 
 %% Plotting
 
 % This is a useful way to evaluate the differences between a set of runs
-name_set = sprintf('%s_M0*_MPhr*',fjord_keys{id});
-plot_subset_runs([outs_path,name_set,'.mat'],'Effect of icebergs','Set2')
+name_set = sprintf('%s_M0*',fjord_keys{id});
+plot_subset_runs([exp_out_path,name_set,'.mat'],'Effect of icebergs','Set2')
 
 % Individual plots for all simulations performed
 chosen_depths=[5, 200, 500];
 for i_param=1:length(M0_space)
     name_exp = sprintf('%s_M0%0.1e',fjord_keys{id},M0_space(i_param));    
-    fjord_run = load([outs_path,name_exp,'.mat']).fjord_run;
+    fjord_run = load([exp_out_path,name_exp,'.mat']).fjord_run;
     hf=plot_ts_at_depth(fjord_run,chosen_depths,'nearest');
-    % print([figs_path,'iceberg_test/',name_exp],'-dpng','-r300')
-    % close gcf;
+    % exportgraphics(gcf,[figs_path,'/',name_set,'.pdf'],'ContentType','vector','BackgroundColor','none'); close gcf;
     % animate(fjord_run,figs_path,name_exp,50);
 end
--- exps_parametre_space.m
+++ exps_parametre_space.m
@@ -1,100 +1,82 @@
 %% Exploring the parametre space
-% 3 parametres, 4 values each - 4^3 = 64 runs
-dt_space = [1, 6, 12, 24]/24.; % time step - dt = [0.5,12] hours
-C0_space = [1e3, 5e3, 1e4, 5e4]; % shelf-exchange coefficient - C0 = [1e3,1e5]
-P0_space=[10, 25, 50, 100]; % plume-entrainment coefficient - P0 = [10,25,50,100] m
-wm_space=[3e-5,2e-5,1e-5,1e-6]; % maximum vertical mixing velocity (default: 4e-5)
-K0_space=[5e-10, 5e-5, 5e-3]; % mixing coefficient (default 0.05)
-tau_space = [5, 10, 30, 90, 365]; % relaxation time - trelax = [10, 3650] days
+id=1;
+[p_def,~] = get_model_default_parameters();
+if id==2 || id==4 % we remove the sill for Sermilik and Ilulissat
+    p_def.sill=0;
+end
+C0_space  = [1e4, 1e3, 5e3, 5e4];      % shelf-exchange coefficient (default: 1e4)
+P0_space  = [25, 10 50, 75];           % plume-entrainment coefficient (default: 25)
+K0_space  = [5e-2, 5e-10, 5e-5, 5e-3]; % mixing coefficient (default 0.05)
+tau_space = [5, 10, 30, 90, 365];      % relaxation time
 
-% test K0 and wmax?
-% n_dt=length(dt_space); n_C0=length(C0_space); n_P0=length(C0_space); n_wm=length(wm_space); n_K0=length(K0_space);
+exp_out_path=[outs_path,'/sens_rlx_time/'];
+mkdir(exp_out_path)
 
 %% Single-parametre run - relaxation time
 for i_tau=1:length(tau_space)
-    name_exp = sprintf('%s_tau%dd_fixed',fjord_keys{id},tau_space(i_tau));    
+    name_exp = sprintf('%s_tau%dd',fjord_keys{id},tau_space(i_tau));    
     fprintf('Running model for %s... ',name_exp)
-    fjord_run = prepare_boxmodel_input(datasets,fjords_processed(fjord_ids(id)),fjord_ids(id)); % arranges into the boxmodel input structure    
-    fjord_run.p.trelax = tau_space(i_tau)*86400;
+    fjord_run = prepare_boxmodel_input(datasets,fjords_processed(fjord_ids(id)),fjord_ids(id),p_def); % arranges into the boxmodel input structure    
+    fjord_run.p.trelax = tau_space(i_tau);
     tic
-    fjord_run.s = boxmodel_v4(fjord_run.p,fjord_run.f,fjord_run.a,fjord_run.t); % runs and gets the results    
+    [fjord_run.s,fjord_run.f] = boxmodel(fjord_run.p,fjord_run.f,fjord_run.a,fjord_run.t); % runs and gets the results    
     if ~fjord_run.s.status, disp('Done.'); end
     toc    
     fjord_run.o = postprocess_boxmodel(fjord_run);
     fjord_run.m.name = name_exp;   
-    save([outs_path,name_exp,'.mat'],'fjord_run','-v7.3') % v7.3 allows files > 2GB
-    % plot_outputs(fjord_run)
+    save([exp_out_path,name_exp,'.mat'],'fjord_run','-v7.3') % v7.3 allows files > 2GB
 end
 
-% Control run:
-fjord_run = prepare_boxmodel_input(datasets,fjords_processed(fjord_ids(id)),fjord_ids(id)); % arranges into the boxmodel input structure    
-fjord_run.p.trelax = NaN;
-fjord_run.s = boxmodel_v4(fjord_run.p,fjord_run.f,fjord_run.a,fjord_run.t); % runs and gets the results    
-fjord_run.o = postprocess_boxmodel(fjord_run);
-fjord_run.m.name = sprintf('%s_tauoff',fjord_keys{id});
-save([outs_path,name_exp,'.mat'],'fjord_run','-v7.3') % v7.3 allows files > 2GB
-hf=plot_ts_at_depth(fjord_run,[5,200,500],'nearest');
-print([figs_path,'rlx_test/',fjord_run.m.name],'-dpng','-r300')
-
+% Plotting outputs
 name_set = sprintf('%s_tau*',fjord_keys{id});
-plot_subset_runs([outs_path,name_set,'.mat'],'Effect of relaxation','Paired')
+plot_subset_runs([exp_out_path,name_set,'.mat'],'Effect of relaxation time','Paired')
 
 for i_tau=1:length(tau_space)
     name_exp = sprintf('%s_tau%dd',fjord_keys{id},tau_space(i_tau));    
-    fjord_run = load([outs_path,name_exp,'.mat']).fjord_run;
+    fjord_run = load([exp_out_path,name_exp,'.mat']).fjord_run;
     hf=plot_ts_at_depth(fjord_run,[5,200,500],'nearest');
-    print([figs_path,'rlx_test/',name_exp],'-dpng','-r300')
-    close gcf;
+    % exportgraphics(gcf,[figs_path,'/',name_exp,'.pdf'],'ContentType','vector','BackgroundColor','none'); close gcf
 end
 
 
 %% Multi-parametre runs
+exp_out_path=[outs_path,'/sens_multi_params/'];
+mkdir(exp_out_path)
 id=1; % we want to focus on Kangerlussuaq (id=1, KF) and Sermilik (id=2, SF)
-for i_C0=1:n_C0
-for i_P0=1:n_P0
-    dt_run = dt_space(3);
-    % name_exp = sprintf('%s_K0%1.e_wm%1.e_C%1.e_P%d_dt%0.2f',fjord_keys{id},K0_space(i_K0),wm_space(i_wm),C0_space(i_C0),P0_space(2),dt_space(1));    
-    name_exp = sprintf('%s_C%1.e_P%d_dt%0.2f_nudged',fjord_keys{id},C0_space(i_C0),P0_space(i_P0),dt_run);    
+for i_C0=1:length(C0_space)
+for i_P0=1:length(P0_space)
+for i_K0=1:length(K0_space)
+    name_exp = sprintf('%s_K%1.e_C%1.e_P%d',fjord_keys{id},K0_space(i_K0),C0_space(i_C0),P0_space(i_P0));    
     fprintf('Running model for %s... ',name_exp)
-    datasets.opts.dt = dt_run;
-    fjord_run = prepare_boxmodel_input(datasets,fjords_processed(fjord_ids(id)),fjord_ids(id)); % arranges into the boxmodel input structure
+    fjord_run = prepare_boxmodel_input(datasets,fjords_processed(fjord_ids(id)),fjord_ids(id),p_def); % arranges into the boxmodel input structure
     fjord_run.p.P0=P0_space(i_P0);
     fjord_run.p.C0=C0_space(i_C0);
-    fjord_run.p.trelax = 10*86400;
+    fjord_run.p.K0=K0_space(i_K0);
     tic
-    fjord_run.s = boxmodel_v4(fjord_run.p,fjord_run.f,fjord_run.a,fjord_run.t); % runs and gets the results    
+    [fjord_run.s,fjord_run.f] = boxmodel(fjord_run.p,fjord_run.f,fjord_run.a,fjord_run.t); % runs and gets the results    
     if ~fjord_run.s.status, disp('Done.'); end
     toc    
     fjord_run.m.name = name_exp;   
-    save([outs_path,name_exp,'.mat'],'fjord_run','-v7.3') % v7.3 allows files > 2GB
-    % plot_outputs(fjord_run)
+    save([exp_out_path,name_exp,'.mat'],'fjord_run','-v7.3') % v7.3 allows files > 2GB
 end
 end
+end
 
-name_ctrl = sprintf('%s_C%1.e_P%d_dt%0.2f',fjord_keys{id},C0_space(3),P0_space(2),dt_space(3));
-% name_ctrl = sprintf('KF_C%1.e_P%d_dt%0.2f',C0_space(3),P0_space(2),dt_space(3));
-fjord_ctrl = load([outs_path,'no_nudge/',name_ctrl]).fjord_run;
-plot_outputs([],fjord_ctrl)
-animate_v4p1(fjord_ctrl,figs_path,name_ctrl,50);
+% Plotting outputs
+name_ctrl = sprintf('%s_K%1.e_C%1.e_P%d',fjord_keys{id},K0_space(1),C0_space(1),P0_space(1));    
+fjord_ctrl = load([exp_out_path,name_ctrl,'.mat']).fjord_run;
+plot_outputs(fjord_ctrl);
+% exportgraphics(gcf,[figs_path,'/',name_ctrl,'.pdf'],'ContentType','vector','BackgroundColor','none');
+% animate([exp_out_path,name_ctrl,'.mat'],figs_path,name_ctrl,50);
 
-% Checking nudging
-name_ctrl_nudge = sprintf('%s_C%1.e_P%d_dt%0.2f_nudged',fjord_keys{id},C0_space(3),P0_space(2),dt_space(3));
-fjord_ctrl_nudge = load([outs_path,name_ctrl_nudge]).fjord_run;
-plot_outputs([],fjord_ctrl_nudge)
+name_set = sprintf('%s_K*_C%1.e_P%d',fjord_keys{id},C0_space(1),P0_space(1)); % varying K0 with default params
+plot_subset_runs([exp_out_path,name_set,'.mat'],'Effect of K_0');
+% exportgraphics(gcf,[figs_path,'/',name_set,'.pdf'],'ContentType','vector','BackgroundColor','none');
 
+name_set = sprintf('%s_K%1.e_C*_P%d',fjord_keys{id},K0_space(1),P0_space(1)); % varying C0 with default params
+plot_subset_runs([exp_out_path,name_set,'.mat'],'Effect of C_0');
+% exportgraphics(gcf,[figs_path,'/',name_set,'.pdf'],'ContentType','vector','BackgroundColor','none');
 
-name_set = sprintf('%s_C%1.e_P%d_dt*',fjord_keys{id},C0_space(3),P0_space(2));    % varying dt with default params
-plot_subset_runs([outs_path,name_set,'.mat'],'Effect of dt')
-
-name_set = sprintf('%s_C*_P%d_dt%0.2f',fjord_keys{id},P0_space(2),dt_space(4));   % varying C0 with default params
-plot_subset_runs([outs_path,name_set,'.mat'],'Effect of C_0')
-
-name_set = sprintf('%s_C%1.e_P*_dt%0.2f',fjord_keys{id},C0_space(3),dt_space(4)); % varying P0 with default params
-plot_subset_runs([outs_path,name_set,'.mat'],'Effect of P_0')
-
-% Checking vertical mixing
-name_set = sprintf('%s_wm*_C%1.e_P%d_dt%0.2f',fjord_keys{id},C0_space(3),P0_space(2),dt_space(1)); % varying wmax with default params
-plot_subset_runs([outs_path,name_set,'.mat'],'Effect of wmax')
-
-name_set = sprintf('%s_K0*wm*_C%1.e_P%d_dt%0.2f',fjord_keys{id},C0_space(3),P0_space(2),0.05/24); % varying wmax and K0 with default params and low time step
-plot_subset_runs([outs_path,name_set,'.mat'],'Effect of K0-wmax')
+name_set = sprintf('%s_K%1.e_C%1.e_P*',fjord_keys{id},K0_space(1),C0_space(1)); % varying P0 with default params
+plot_subset_runs([exp_out_path,name_set,'.mat'],'Effect of P_0');
+% exportgraphics(gcf,[figs_path,'/',name_set,'.pdf'],'ContentType','vector','BackgroundColor','none');
