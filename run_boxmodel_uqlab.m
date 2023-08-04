34b1f90eb29c3e0cd879a4efc314abca4777bc61
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-08-04 10:20:31 +0100

    function to define the input PDFs, common parametres, and run the wrapper within UQLab (WiP)

---
+++ run_boxmodel_uqlab.m
@@ -0,0 +1,61 @@
+%% Sets up to run the box model within the UQLab framework
+
+%% Configuring paths
+run load_local_paths.m % sets data_path, import_path, collation_path, model_path, and project_path
+addpath(genpath(import_path))
+addpath(genpath(model_path))
+addpath(genpath(collation_path))
+
+[datasets,fjords_compilation,~,~] = compile_datasets(data_path);
+
+% outs_path = [data_path,'/greenland/FjordMIX/boxmodel/pce/']; % where the model output files will be saved
+% figs_path = [project_path,'/figs/pce/'];                     % where the figures and animations will be saved
+
+
+%% evaluate the wrapper by itself first
+[Parameters,IOpts,probs] = define_model_param_distrib(datasets,fjords_compilation,1);
+
+rng('default')
+status=1;
+while status==1
+try
+    X = zeros(size(probs));
+    for i=1:length(probs)
+        X(i) = random(probs(i));
+    end    
+    [ohc,osc,status] = wrapper_boxmodel(X,Parameters);
+catch ME
+    disp('Unsuccessful run')
+end
+end
+% check of Qsg "anomalies" 
+for i=1:7 
+    [Parameters,IOpts,probs] = define_model_param_distrib(datasets,fjords_compilation,i);
+    figure; hold on; 
+    histogram(random(probs(8),10000)); 
+end
+
+%% Setting up the runs per region
+
+for i_reg=1:1
+    [Parameters,IOpts,~] = define_model_param_distrib(datasets,fjords_compilation,i_reg);
+
+    MOpts.mFile = 'wrapper_boxmodel';
+    MOpts.isVectorized = false;
+    
+    % Specification of 14th degree LARS−based PCE     
+    MOpts.Type = 'Metamodel';
+    MOpts.MetaType = 'PCE';    
+    MOpts.Method = 'LARS';
+    MOpts.Degree = 14;
+    MOpts.ExpDesign.NSamples = 1000;
+    model = uq_createModel(MOpts);
+    
+    input = uq_getInput(IOpts);
+    X = uq_getSample(input,500,'LHS');
+    
+    Y = uq_evalModel(model,X);
+end
+
+uq_figure
+uq_histogram(Y)
