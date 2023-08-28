b95c8c6a680c6e3aa204ce0ab953186a542269bc
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-08-28 15:30:14 +0100

    functional trial MC test added to monitor how much the model crashes

--- run_boxmodel_uqlab.m
+++ run_boxmodel_uqlab.m
@@ -6,6 +6,7 @@ addpath(genpath(import_path))
 addpath(genpath(model_path))
 addpath(genpath(collation_path))
 addpath(genpath(uqlab_path))
+addpath(genpath('./'))
 
 [datasets,fjords_compilation,~,~] = compile_datasets(data_path);
 
@@ -13,36 +14,68 @@ outs_path = [data_path,'/greenland/FjordMIX/boxmodel/pce/']; % where the model o
 figs_path = [project_path,'/figs/pce/'];                     % where the figures and animations will be saved
 
 %% Showing all input parameters first
-hf = plot_distributions(datasets,fjords_compilation);
-exportgraphics(hf,[figs_path,'summary_input_params.png'],'Resolution',300)
+% hf = plot_distributions(datasets,fjords_compilation);
+% exportgraphics(hf,[figs_path,'summary_input_params.png'],'Resolution',300)
 
 %% evaluate the wrapper by itself first
 [Parameters,IOpts,probs,fjords_processed] = define_model_param_distrib(datasets,fjords_compilation,1);
 
 rng('default')
-n_runs = 10;
-clear ensemble fjord run
-ensemble(length(n_runs)) = struct('time',[],'ohc',[],'osc',[]);
-% ohc_out = NaN([n_runs, 1]);
-for k=1:n_runs
-try
-    X = zeros(size(probs));
-    for i=1:length(probs)
-        X(i) = random(probs(i));
-    end    
-    tic
-    [ensemble(k).time,ensemble(k).ohc,ensemble(k).osc,status,fjord_run(k)] = wrapper_boxmodel(X,Parameters);
-    % ohc_out(k) = wrapper_boxmodel(X,Parameters);
-    toc
-catch ME
-    fprintf('Crash on iteration #%d\n',k)
-    if ~isempty(ME.stack) > 0
-        fprintf('Something went wrong on %s line %d\n',ME.stack(1).name,ME.stack(1).line)
-        fprintf('The error message was:\n%s\n',ME.message)
+n_runs = 30;
+clear ensemble fjord_run
+% ensemble(length(n_runs)) = struct('time',[],'ohc',[],'osc',[]);
+
+ohc_pd = cell([7,1]);
+ohc_out = NaN([n_runs, 7]);
+osc_pd = cell([7,1]);
+osc_out = NaN([n_runs, 7]);
+for i_reg=1:7    
+    for k=1:n_runs
+        try
+            X = zeros(size(probs));
+            for i=1:length(probs)
+                X(i) = random(probs(i));
+            end    
+            tic
+            % [ensemble(k).time,ensemble(k).ohc,ensemble(k).osc,status,fjord_run(k)] = wrapper_boxmodel(X,Parameters);
+            [ohc_out(k,i_reg),osc_out(k,i_reg)] = wrapper_boxmodel(X,Parameters);
+            toc
+            ohc_pd{i_reg} = makedist('Normal','mu',mean(ohc_out(:,i_reg),'omitnan'),'sigma',std(ohc_out(:,i_reg),'omitnan'));
+            osc_pd{i_reg} = makedist('Normal','mu',mean(osc_out(:,i_reg),'omitnan'),'sigma',std(osc_out(:,i_reg),'omitnan'));
+        catch ME
+            fprintf('Crash on iteration #%d\n',k)
+            ohc_out(k,i_reg) = NaN;
+            osc_out(k,i_reg) = NaN;
+        end
     end
 end
+
+% Plotting the results
+ok_runs = zeros([1, 7]);
+regions = {'SW','SE','CW','CE','NW','NE','NO'};
+for i_reg=1:7
+    total_runs = ohc_out(:,i_reg);
+    ok_runs(i_reg) = sum(~isnan(total_runs));
+    regions{i_reg} = [regions{i_reg},' ( n=',num2str(ok_runs(i_reg)),')'];
 end
-figure; hold on; for k=1:n_runs, plot(ensemble(k).time,ensemble(k).ohc); end
+ohc_x = linspace(400,1600,1000);
+osc_x = linspace(3.4,4,1000);
+figure('Position',[40 40 850 300]); 
+subplot(1,2,1), hold on; for i_reg=1:7, plot(ohc_x,pdf(ohc_pd{i_reg},ohc_x),'linewidth',2); end
+xlabel('Heat content (J m^{-3})',fontsize=14); ylabel('Probability',fontsize=14);  box on
+text(0.05,0.95,'(a)','fontsize',14,'units','normalized')
+set(gca,'fontsize',14)
+xlim([800 1600]);
+subplot(1,2,2), hold on; for i_reg=1:7, plot(osc_x,pdf(osc_pd{i_reg},osc_x),'linewidth',2); end
+xlabel('Salt content (g m^{-3})',fontsize=14); box on
+text(0.05,0.95,'(b)','fontsize',14,'units','normalized')
+set(gca,'fontsize',14)
+xlim([3.4 4]);
+legend(regions,'fontsize',14)
+exportgraphics(gcf,[figs_path,'test_output_ohc_osc_pdfs.png'],'Resolution',300)
+
+
+% figure; hold on; for k=1:n_runs, plot(ensemble(k).time,ensemble(k).ohc); end
 
 %% Setting up the runs per region
 
@@ -50,7 +83,7 @@ figure; hold on; for k=1:n_runs, plot(ensemble(k).time,ensemble(k).ohc); end
 uqlab
 for i_reg=1:1
     [Parameters,IOpts,~,~] = define_model_param_distrib(datasets,fjords_compilation,i_reg);
-
+    rng('default')
     % create numerical model object
     ModelOpts.mFile = 'wrapper_boxmodel';
     ModelOpts.isVectorized = false;
@@ -59,7 +92,7 @@ for i_reg=1:1
     
     % create inputs object
     input = uq_createInput(IOpts);
-    X = uq_getSample(input,10,'LHS');
+    X = uq_getSample(input,50,'LHS');
 
     % create Meta (surrogate) Model: specification of 14th degree LARS−based PCE     
     MetaOpts.Type = 'Metamodel';
@@ -67,15 +100,15 @@ for i_reg=1:1
     MetaOpts.FullModel = num_model;
     MetaOpts.Method = 'LARS';
     MetaOpts.Degree = 14;
-    MetaOpts.ExpDesign.NSamples = 10;
+    MetaOpts.ExpDesign.NSamples = 50;
     sur_model=uq_createModel(MetaOpts);
     % uq_print(sur_model)
     % uq_display(sur_model)
-
     Y = uq_evalModel(sur_model,X);
     Yvalidation = uq_evalModel(num_model,X);
     uq_plot(Yvalidation,Y,'+')
 end
+
 % 
 % uq_figure
 % uq_histogram(Y)
