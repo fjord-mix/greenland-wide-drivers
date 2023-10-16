0ebef157cbd626a2cc0a544a3f607d96a2420328
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-10-16 10:42:23 +0100

    now returns whole time series as opposed to a single value

--- wrapper_boxmodel.m
+++ wrapper_boxmodel.m
@@ -1,4 +1,5 @@
-function [ohc_mean,osc_mean] = wrapper_boxmodel(X,Parameters)
+function [output_time,heat_content,salt_content] = wrapper_boxmodel(X,Parameters,flag_debug)
+% function [ohc_mean,osc_mean] = wrapper_boxmodel(X,Parameters,flag_debug)
 % function [output_time,heat_content,salt_content,status,fjord_run] = wrapper_boxmodel(X,Parameters)
 % Wrapper function for running the boxmodel in UQLab
 % X contains all parameters we want to explore (M=8)
@@ -6,7 +7,7 @@ function [ohc_mean,osc_mean] = wrapper_boxmodel(X,Parameters)
 % runs
 [p,a] = get_model_default_parameters(); % default params, standard initialisation
 p.H = Parameters.H;
-p.trelax=365/2;
+% p.trelax=365/2;
 p.Hmin=5;
 p.M0=0;
 p.P0=X(10);
@@ -57,8 +58,8 @@ end
 % Alternative: evenly distribute layer thicknesses above the sill
 % a.H0 = ones([1,p.N]).*(abs(p.silldepth.*p.sill)/p.N);
 % if p.sill, a.H0 = [a.H0,p.H-abs(p.silldepth)]; end
-
 [a.T0, a.S0] = bin_ocean_profiles(Ts(:,1),Ss(:,1),zs,a.H0,p); 
+
 p.Snudge = get_interface_salinities(zs,mean(Ts,2),mean(Ss,2),p);
 
 % Glacier forcings
@@ -69,7 +70,9 @@ f.xi = Parameters.xi;
 a.I0 = Parameters.I0;
 
 %% For plotting purposes
-p.plot_runtime=0;
+if nargin > 2
+    p.plot_runtime=flag_debug; % for debugging purposes
+end
 % run figure_forcings_summary.m
 
 %% Preparing fjord encapsulating structure
@@ -80,7 +83,6 @@ fjord_run.a = a;
 
 %% Run the model itself, postprocess, and return the desired metrics for evaluation
 
-
 [fjord_run.s,fjord_run.f] = boxmodel(fjord_run.p, fjord_run.t, fjord_run.f, fjord_run.a);
 if fjord_run.s.status
     disp('this is a crash example')
@@ -90,14 +92,20 @@ fjord_run.o = postprocess_boxmodel(fjord_run);
 % gets output quantities in "per unit volume"
 heat_content = sum(fjord_run.o.hc)./(fjord_run.p.L.*fjord_run.p.W.*fjord_run.p.H);
 salt_content = sum(fjord_run.o.sc)./(fjord_run.p.L.*fjord_run.p.W.*fjord_run.p.H);
-if length(heat_content) > 365
-    ohc_mean = mean(heat_content(end-365:end)); % get the mean for the last year
-    osc_mean = mean(salt_content(end-365:end));
-else % if the time series is too short
-    % ohc_mean = mean(heat_content); % get the mean for the whole period
-    % osc_mean = mean(salt_content);
-    ohc_mean = NaN;
-    osc_mean = NaN;
-end
+output_time = fjord_run.s.t;
+% if length(heat_content) > 365*2
+%     ohc_start = mean(heat_content(1:365)); % get the mean for the last year
+%     osc_start = mean(salt_content(1:365));
+% 
+%     ohc_end = mean(heat_content(end-365:end)); % get the mean for the last year
+%     osc_end = mean(salt_content(end-365:end));
+% 
+%     % get the difference to account for total warming/freshening
+%     ohc_mean = ohc_end-ohc_start; 
+%     osc_mean = osc_end-osc_start;
+% else % if the time series is too short
+%     ohc_mean = NaN;
+%     osc_mean = NaN;
+% end
 
 end
