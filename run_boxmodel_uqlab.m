cbe38dd423d47ac2566c241aa05d08cd9aaffcf0
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-10-16 10:52:13 +0100

    updated to reflect time-dependent run changes and extra tests

--- run_boxmodel_uqlab.m
+++ run_boxmodel_uqlab.m
@@ -8,32 +8,33 @@ addpath(genpath(collation_path))
 addpath(genpath(uqlab_path))
 addpath(genpath('./'))
 
-[datasets,fjords_compilation,~,~] = compile_datasets(data_path);
+[datasets,fjords_compilation,fjords_map,~] = compile_datasets(data_path);
 outs_path = [data_path,'/greenland/FjordMIX/boxmodel/pce/']; % where the model output files will be saved
 figs_path = [project_path,'/figs/pce/'];                     % where the figures and animations will be saved
 letters = {'a','b','c','d','e','f','g','h'};
 
 %% Showing all input parameters first
+% hs = plot_fjords_summary(datasets,fjords_map,fjords_compilation); %plt_handles.cb1.Visible = 'off'; plt_handles.cb2.Visible = 'off'; plt_handles.cb3.Visible = 'off'; 
 % hf = plot_distributions(datasets,fjords_compilation);
-% exportgraphics(hf,[figs_path,'summary_input_params.png'],'Resolution',300)
+% exportgraphics(hf,[figs_path,'summary_input_params2010-2018.png'],'Resolution',300)
 
 %% Initialise all needed variables
 regions = {'SW','SE','CW','CE','NW','NE','NO'};
-n_runs = 50;
+n_runs    = 50;
+time_step = 0.1; % in days
 n_regions = length(regions);
 
-% X       = zeros([n_runs,10,n_regions]);
 ohc_out = NaN([n_runs, n_regions]);
 osc_out = NaN([n_runs, n_regions]);
+ensemble(n_runs,n_regions) = struct("time",[],"ohc",[],"osc",[]);
 ohc_pd  = cell([1,n_regions]);
 osc_pd  = cell([1,n_regions]);
 ohc_ks  = cell([1, n_regions]);
 osc_ks  = cell([1, n_regions]);
-Parameters = cell([1, n_regions]);
 
-X       = zeros([n_runs,n_regions,10]);
-% ohc_out = zeros([1,n_runs*n_regions]);
-% osc_out = zeros([1,n_runs*n_regions]);
+
+Parameters = cell([1, n_regions]);
+X         = zeros([n_runs,n_regions,10]);
 
 % Initialise UQLab
 uqlab
@@ -41,7 +42,7 @@ uqlab
 %% sample the distributions for inputs
 rng('default') % set the seed for reproducibility
 for i_reg=1:n_regions
-    [Parameters{i_reg},IOpts,~,~] = define_model_param_distrib(datasets,fjords_compilation,i_reg); % available outputs: [Parameters,IOpts,probs,fjords_processed]
+    [Parameters{i_reg},IOpts,~,~] = define_model_param_distrib(datasets,fjords_compilation,i_reg,time_step); % available outputs: [Parameters,IOpts,probs,fjords_processed]
     input = uq_createInput(IOpts);
     X(:,i_reg,:) = uq_getSample(input,n_runs,'LHS'); % perform latin hypercube sampling
 end
@@ -56,28 +57,9 @@ end
 % Using a parfor inside the for loop will likely increase overhead time, 
 % but will significantly reduce the amount of memory used used 
 % by slicing X and Prameters before the parallel runs
+run model_runs_per_region_and_pdfs.m
 
-for i_reg=1:n_regions
-    Xreg = squeeze(X(:,i_reg,:));
-    Params_reg = Parameters{i_reg};
-    tic
-    for k_run=1:n_runs
-        try
-            % [ensemble(k).time,ensemble(k).ohc,ensemble(k).osc,status,fjord_run(k)] = wrapper_boxmodel(X,Parameters);
-            [ohc_out(k_run,i_reg),osc_out(k_run,i_reg)] = wrapper_boxmodel(Xreg(k_run,:),Params_reg);
-        catch ME
-            ohc_out(k_run,i_reg) = NaN;
-            osc_out(k_run,i_reg) = NaN;
-        end
-        fprintf('run %d complete\n',k_run)
-    end
-    toc
-    fprintf('region %d complete\n',i_reg)
-end
-% reshape input/output matrices afterwards to a more tractable format
-% ohc_rsp = reshape(ohc_out,n_runs,n_regions);
-% osc_rsp = reshape(osc_out,n_runs,n_regions);
-% Xrsp    = reshape(X,n_runs,n_regions,10);
+save([outs_path,'hc_sc_ensemble_n',num2str(n_runs)],'-v7.3','ensemble')
 %% Calculate the distributions based on the numerical outputs alone
 
 for i_reg=1:n_regions
@@ -87,8 +69,9 @@ for i_reg=1:n_regions
     osc_ks{i_reg} = fitdist(osc_out(:,i_reg),'kernel');
 end
 % save outputs in case matlab crashes, or so we dont have to re-run it
-save([outs_path,'ohc_osc_runs_probs_n30'],'ohc_out','osc_out','ohc_pd','osc_pd','ohc_ks','osc_ks')
-% load([outs_path,'ohc_osc_runs_probs_n30'])
+save([outs_path,'ohc_osc_change_runs_probs_n',num2str(n_runs)],...
+      'ohc_out','osc_out','ohc_pd','osc_pd','ohc_ks','osc_ks')
+% load([outs_path,'ohc_osc_runs_probs_n',num2str(n_runs)])
 %% Setting up the PCE model per region using UQLab
 
 Ysur_ohc      = cell([1,n_regions]); % surrogate model results evaluated at the same points as the numerical model
@@ -97,21 +80,30 @@ Ynum_ohc      = cell([1,n_regions]); % numerical model results
 Ynum_osc      = cell([1,n_regions]);
 Yeval_ohc     = cell([1,n_regions]); % surrogate model results evaluated at a much larger input range
 Yeval_osc     = cell([1,n_regions]);
-sobolA_ohc    = cell([1,n_regions]);
-sobolA_osc    = cell([1,n_regions]);
 sur_model_ohc = cell([1,n_regions]);
 sur_model_osc = cell([1,n_regions]);
 ohc_ks_eval   = cell([1,n_regions]);
 osc_ks_eval   = cell([1,n_regions]);
 
+sobolA_ohc    = cell([1,n_regions]);
+sobolA_osc    = cell([1,n_regions]);
 SobolOpts.Type             = 'Sensitivity';
 SobolOpts.Method           = 'Sobol';
 SobolOpts.Sobol.Order      = 1;
 SobolSensOpts.SaveEvaluations = false; % to prevent excessive memory usage!
 % SobolOpts.Sobol.SampleSize = 1e5; % only needed for MC, but we are using the PCE model itself
 
+% ANCOVA_ohc    = cell([1,n_regions]);
+% ANCOVA_osc    = cell([1,n_regions]);
+% ANCOVASensOpts_ohc.Type = 'Sensitivity';
+% ANCOVASensOpts_ohc.Method = 'ANCOVA';
+% ANCOVASensOpts_osc = ANCOVASensOpts_ohc;
+% ANCOVASensOpts.ANCOVA.SampleSize = 150;
+% ANCOVAAnalysis = uq_createAnalysis(ANCOVASensOpts);
+
+
 % Initialise UQLab
-uqlab
+% uqlab
 for i_reg=1:n_regions
     [Params_reg,IOpts,~,~] = define_model_param_distrib(datasets,fjords_compilation,i_reg);
 
@@ -161,6 +153,11 @@ for i_reg=1:n_regions
     
     sobolA_osc{i_reg}  = uq_createAnalysis(SobolOpts);
     osc_ks_eval{i_reg} = fitdist(Yeval_osc{i_reg},'kernel');
+
+    % ANCOVASensOpts_ohc.ANCOVA.CustomPCE = sur_model_ohc{i_reg};
+    % ANCOVASensOpts_osc.ANCOVA.CustomPCE = sur_model_osc{i_reg};
+    % ANCOVA_ohc{i_reg}  = uq_createAnalysis(ANCOVASensOpts_ohc);
+    % ANCOVA_osc{i_reg}  = uq_createAnalysis(ANCOVASensOpts_osc);
     % uq_histogram(Yeval)
 end
 % save([outs_path,'ohc_osc_pce_n50_n1e6'],'sur_model_ohc','sur_model_osc','Ysur_ohc','Ysur_osc','Yeval_ohc','Yeval_osc','sobolA_ohc','sobolA_osc')
