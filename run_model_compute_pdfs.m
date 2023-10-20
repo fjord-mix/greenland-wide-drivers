aad587b87b49c0ea93c9b4c91868ce3199567c55
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-10-20 11:25:03 +0100

    returns full time series to compute total/above/below sill separately

--- run_model_compute_pdfs.m
+++ run_model_compute_pdfs.m
@@ -18,60 +18,56 @@ for i_reg=1:n_regions
     for k_run=1:n_runs
         try
             % [ohc_out(k_run,i_reg),osc_out(k_run,i_reg)] = wrapper_boxmodel(Xreg(k_run,:),Params_reg);
-            [ensemble(k_run,i_reg).time,ensemble(k_run,i_reg).ohc,ensemble(k_run,i_reg).osc,...
-                                        ensemble(k_run,i_reg).ohc_as,ensemble(k_run,i_reg).osc_as] = ...
-             wrapper_boxmodel(Xreg(k_run,:),Params_reg);
+            [ensemble(k_run,i_reg).time,ensemble(k_run,i_reg).ohc,ensemble(k_run,i_reg).osc,ensemble(k_run,i_reg).p] = wrapper_boxmodel(Xreg(k_run,:),Params_reg);
             fprintf('run %d complete\n',k_run)
         catch ME
             ensemble(k_run,i_reg).ohc = NaN(size(ensemble(k_run,i_reg).time));
             ensemble(k_run,i_reg).osc = NaN(size(ensemble(k_run,i_reg).time));
-            ensemble(k_run,i_reg).ohc_as = NaN(size(ensemble(k_run,i_reg).time));
-            ensemble(k_run,i_reg).osc_as = NaN(size(ensemble(k_run,i_reg).time));
             fprintf('run %d %s\n',k_run,ME.message)
-        end
-        
-        if length(ensemble(k_run,i_reg).ohc) > 365*2
-            ohc_start = mean(ensemble(k_run,i_reg).ohc(1:365)); % get the mean for the last year
-            osc_start = mean(ensemble(k_run,i_reg).osc(1:365));
-        
-            ohc_end = mean(ensemble(k_run,i_reg).ohc(end-365:end)); % get the mean for the last year
-            osc_end = mean(ensemble(k_run,i_reg).osc(end-365:end));
-        
-            % get the difference to account for total warming/freshening
-            ohc_out(k_run,i_reg) = ohc_end-ohc_start; 
-            osc_out(k_run,i_reg) = osc_end-osc_start;
-
-            % Computing for only layers above the sill
-            ohc_start_as = mean(ensemble(k_run,i_reg).ohc_as(1:365)); 
-            osc_start_as = mean(ensemble(k_run,i_reg).osc_as(1:365));
-            ohc_end_as   = mean(ensemble(k_run,i_reg).ohc_as(end-365:end)); 
-            osc_end_as   = mean(ensemble(k_run,i_reg).osc_as(end-365:end));
-            ohc_out_as(k_run,i_reg) = ohc_end_as-ohc_start_as; 
-            osc_out_as(k_run,i_reg) = osc_end_as-osc_start_as;
-        else 
-            % if the time series is too short, i.e., the model crashed
-            ohc_out(k_run,i_reg) = NaN;
-            osc_out(k_run,i_reg) = NaN;
-            ohc_out_as(k_run,i_reg) = NaN;
-            osc_out_as(k_run,i_reg) = NaN;
         end
     end
     toc
     fprintf('region %d complete\n',i_reg)
 end
+fprintf('Model runs complete. Starting computation of heat/salt contents...\n')
+time_axis = datetime(2010,01,15)+1:1:datetime(2018,12,15);
+for i_reg=1:n_regions
+    for k_run=1:n_runs
+        if length(ensemble(k_run,i_reg).ohc) == length(time_axis)
+            
+            % compute quantities per unit volume
+            fjord_run = ensemble(k_run,i_reg);
+            heat_content = sum(fjord_run.ohc,1)./(fjord_run.p.L.*fjord_run.p.W.*fjord_run.p.H);
+            salt_content = sum(fjord_run.osc,1)./(fjord_run.p.L.*fjord_run.p.W.*fjord_run.p.H);
+
+            ohc_start = mean(heat_content(1:365)); % get the mean for the first year
+            osc_start = mean(salt_content(1:365));
+        
+            ohc_end = mean(heat_content(end-365:end)); % get the mean for the last year
+            osc_end = mean(salt_content(end-365:end));
+        
+            % get the difference to account for total warming/freshening
+            ohc_out(k_run,i_reg) = ohc_end-ohc_start; 
+            osc_out(k_run,i_reg) = osc_end-osc_start;
+        else 
+            % if the time series is too short, i.e., the model crashed
+            ohc_out(k_run,i_reg) = NaN;
+            osc_out(k_run,i_reg) = NaN;
+        end
+    end
+end
+fprintf('Computation of heat/salt contents complete.\n')
 
 %% Calculate the distributions based on the numerical outputs alone
 % this is just for comparison with the surrogate model outputs
 % load([outs_path,'ohc_osc_runs_probs_n',num2str(n_runs)]) % if we have the results saved already
 
-for i_reg=1:n_regions
-    ohc_pd{i_reg} = makedist('Normal','mu',mean(ohc_out(:,i_reg),'omitnan'),'sigma',std(ohc_out(:,i_reg),'omitnan'));
-    osc_pd{i_reg} = makedist('Normal','mu',mean(osc_out(:,i_reg),'omitnan'),'sigma',std(osc_out(:,i_reg),'omitnan'));
-    ohc_ks{i_reg} = fitdist(ohc_out(:,i_reg),'kernel');
-    osc_ks{i_reg} = fitdist(osc_out(:,i_reg),'kernel');
-    ohc_ks_as{i_reg} = fitdist(ohc_out_as(:,i_reg),'kernel');
-    osc_ks_as{i_reg} = fitdist(osc_out_as(:,i_reg),'kernel');
-end
+% for i_reg=1:n_regions
+%     ohc_pd{i_reg} = makedist('Normal','mu',mean(ohc_out(:,i_reg),'omitnan'),'sigma',std(ohc_out(:,i_reg),'omitnan'));
+%     osc_pd{i_reg} = makedist('Normal','mu',mean(osc_out(:,i_reg),'omitnan'),'sigma',std(osc_out(:,i_reg),'omitnan'));
+%     ohc_ks{i_reg} = fitdist(ohc_out(:,i_reg),'kernel');
+%     osc_ks{i_reg} = fitdist(osc_out(:,i_reg),'kernel');
+% end
 
 %% Separate tests of specific runs for debugging model instabilities
 % wrapper_boxmodel(X(36,1,:),Parameters{1});
