01b37ebac6b31aa06edc072aaa2c5c3918a5c590
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-11-29 15:31:09 +0000

    code cleaning and added more info on Borgonovo vs Sobol

--- make_figs_uqlab_outputs.m
+++ make_figs_uqlab_outputs.m
@@ -1,4 +1,17 @@
-%% Plotting the results of the numerical model alone
+%% Showing all input parameters first
+
+% close all
+% plot_reg_ocn_forcings(datasets,fjords_compilation)
+% figure(1); exportgraphics(gcf,[figs_path,'hovmoller_Ts.png'],'Resolution',300)
+% figure(2); exportgraphics(gcf,[figs_path,'hovmoller_Ss.png'],'Resolution',300)
+% figure(3); exportgraphics(gcf,[figs_path,'series_discharge_hc_sc.png'],'Resolution',300)
+
+% hs = plot_fjords_summary(datasets,fjords_map,fjords_compilation); %plt_handles.cb1.Visible = 'off'; plt_handles.cb2.Visible = 'off'; plt_handles.cb3.Visible = 'off'; 
+% hf = plot_distributions(datasets,fjords_compilation);
+% exportgraphics(hf,[figs_path,'summary_input_probs2010-2018_ratios.png'],'Resolution',300)
+
+
+%% Plotting the results of our analyses
 
 % we want to know how many runs were successful
 ok_runs  = zeros([1, n_regions]);
@@ -13,7 +26,10 @@ for i_reg=1:n_regions
     ok_vruns(i_reg) = sum(~isnan(total_runs));
 end
 
-% get the range of results for computing the probability distributions
+% time axis for plotting the results, excluding t0
+time_axis_plt = time_axis(2:end); % datetime(2010,01,15)+1:1:datetime(2018,12,15); 
+
+% get the range of results for showing the probability distributions
 ohc_x = linspace(1.2*min(ohc_out(:)),1.2*max(ohc_out(:)),1000);
 osc_x = linspace(1.2*min(osc_out(:)),1.2*max(osc_out(:)),1000);
 
@@ -21,13 +37,6 @@ osc_x = linspace(1.2*min(osc_out(:)),1.2*max(osc_out(:)),1000);
 % datasets.opts.time_start = time_axis(1);
 % datasets.opts.time_end   = time_axis(end);
 % datasets.opts.dt         = 30.;
-% fjords_processed(size(fjords_compilation)) = struct("p",[],"a",[],"f",[],"t",[],"m",[]);
-% for i=1:length(fjords_compilation),fjords_processed(i) = prepare_boxmodel_input(datasets,fjords_compilation(i));end
-
-
-
-region_line_color = lines(7);
-time_axis_plt = datetime(2010,01,15)+1:1:datetime(2018,12,15);
 
 %% Plot the ocean forcing (and its variability) for each region
 plot_reg_ocn_profiles(datasets,fjords_compilation)
@@ -58,7 +67,30 @@ plot_model_fits(Ynum_osc,Ysur_osc,Yind_osc,Yvld_osc,ok_runs,ok_vruns,'salinity',
 [hf,hp,hl] = plot_numerical_model_distributions(ohc_x,osc_x,ohc_ks,osc_ks,regions_lbl);
 % exportgraphics(gcf,[figs_path,'ksnum_ohc_osc_n',num2str(n_runs),'.png'],'Resolution',300)
 
-%% Plotting the Sobol indices
+%% Plotting the Borgonovo Indices (quantifying role of inputs in each region's fjord-shelf differences for each region)
+% Original publication: https://doi.org/10.1016/j.ress.2006.04.015
+figure('Name','Borgonovo Indices','position',[40 40 1000 400])
+for i_reg=1:7
+    borgResults_ohc  = BorgonovoA_ohc{i_reg}.Results;
+    borgResults_osc  = BorgonovoA_osc{i_reg}.Results;
+    borgIndices = [borgResults_ohc.Delta borgResults_osc.Delta];
+
+    subplot(2,4,i_reg); hold on; box on; grid on
+    hb=uq_bar(gca,1:length(IOpts{i_reg}.Marginals), borgIndices, 1.,'grouped');
+    text(0.01,1.075,sprintf('(%s) %s',letters{i_reg},regions{i_reg}),'units','normalized','fontsize',12)
+    set(gca,'XTick', 1:length(IOpts{i_reg}.Marginals),'XTickLabel', borgResults_ohc.VariableNames)
+    if i_reg < 4, xlabel(''); end
+    if i_reg==1 || i_reg==5, ylabel('Borgonovo indices'); end
+    ylim([0 0.5])
+end
+hl = legend(hb,{'Temperature','Salinity'},'fontsize',12);
+hl.Position(1)=hl.Position(1)+0.175;
+
+% for i_reg=1:7
+%     uq_display(BorgonovoA_ohc{1},1,'Joint PDF',3);
+% end
+
+%% Plotting the Sobol indices (quantifying role of inputs in the variability between fjords in the same region)
 
 figure('Name','First-order Sobol Indices','position',[40 40 1000 400])
 for i_reg=1:7
@@ -97,23 +129,6 @@ hl = legend(hb,{'Temperature','Salinity'},'fontsize',12);
 hl.Position(1)=hl.Position(1)+0.175;
 % exportgraphics(gcf,[figs_path,'sobol_total_n',num2str(n_runs),'.png'],'Resolution',300)
 
-%% Plotting the Borgonovo Indices
-figure('Name','Borgonovo Indices','position',[40 40 1000 400])
-for i_reg=1:7
-    borgResults_ohc  = BorgonovoA_ohc{i_reg}.Results;
-    borgResults_osc  = BorgonovoA_osc{i_reg}.Results;
-    borgIndices = [borgResults_ohc.Delta borgResults_osc.Delta];
-
-    subplot(2,4,i_reg); hold on; box on; grid on
-    hb=uq_bar(gca,1:length(IOpts{i_reg}.Marginals), borgIndices, 1.,'grouped');
-    text(0.01,1.075,sprintf('(%s) %s',letters{i_reg},regions{i_reg}),'units','normalized','fontsize',12)
-    set(gca,'XTick', 1:length(IOpts{i_reg}.Marginals),'XTickLabel', borgResults_ohc.VariableNames)
-    if i_reg < 4, xlabel(''); end
-    if i_reg==1 || i_reg==5, ylabel('Borgonovo indices'); end
-    ylim([0 0.5])
-end
-hl = legend(hb,{'Temperature','Salinity'},'fontsize',12);
-hl.Position(1)=hl.Position(1)+0.175;
 
 %% Convergence test to see if our choice of n_runs was enough
 
