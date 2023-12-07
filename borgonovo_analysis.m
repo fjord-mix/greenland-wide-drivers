cfa8dddda260713d742243581d901c3156c74a73
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2023-12-07 11:44:20 +0000

    removed unecessary comments

--- borgonovo_analysis.m
+++ borgonovo_analysis.m
@@ -6,9 +6,9 @@ BorgonovoOpts.Method = 'Borgonovo';
 
 for i_reg=1:n_regions
     BorgonovoOpts.Borgonovo.Sample.X = Xeval(:,i_reg,:);
-    BorgonovoOpts.Borgonovo.Sample.Y = Yeval_ohc{i_reg}; % likely needs some .something
+    BorgonovoOpts.Borgonovo.Sample.Y = Yeval_ohc{i_reg}; 
     BorgonovoA_ohc{i_reg} = uq_createAnalysis(BorgonovoOpts);
-    BorgonovoOpts.Borgonovo.Sample.Y = Yeval_osc{i_reg}; % likely needs some .something
+    BorgonovoOpts.Borgonovo.Sample.Y = Yeval_osc{i_reg};
     BorgonovoA_osc{i_reg} = uq_createAnalysis(BorgonovoOpts);
 end
 
