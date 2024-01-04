9980959d4b8d6e0fbc244e45be96a75324757281
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-01-04 17:45:30 +0200

    shows model-fit statistics similar to Edwards et al. (2021; Nature) and Seroussi et al. (2023; TC)

--- plotting/plot_model_fits.m
+++ plotting/plot_model_fits.m
@@ -1,4 +1,4 @@
-function hf = plot_model_fits(Ynum,Ysur,Yind,Yvld,ok_runs,ok_vruns,var,units)
+function hf = plot_model_fits(sur_model,Ynum,Ysur,Yind,Yvld,ok_runs,var,units)
 
 letters = {'a','b','c','d','e','f','g','h'};
 regions = {'SW','SE','CW','CE','NW','NE','NO'};
@@ -10,13 +10,21 @@ for i_reg=1:n_regions
     uq_plot(gca,Ynum{i_reg},Ysur{i_reg},'+')
     if ~isempty(Yvld) && ~isempty(Yind)
         uq_plot(gca,Yind{i_reg},Yvld{i_reg},'o','color',[0.8500 0.3250 0.0980])
-        rms = rmse(Yvld{i_reg},Yind{i_reg},'omitnan');
-        text(0.98,0.09,sprintf('RMSE=%0.2f %s',rms,units),'units','normalized','horizontalAlignment','right','fontsize',14)
+        mdl = fitlm(Yind{i_reg},Yvld{i_reg});
+        text(0.98,0.09,sprintf('R^2=%0.2f %s',mdl.Rsquared.Adjusted,units),'units','normalized','horizontalAlignment','right','fontsize',14)
+    else
+        mdl = fitlm(Ynum{i_reg},Ysur{i_reg});
+        norm_err = (Ysur{i_reg}-Ynum{i_reg})./std(Ysur{i_reg});
+        pct_within = sum(abs(norm_err)<2,'omitnan')./length(norm_err)*100;
+        % prctiles = prctile(Ysur{i_reg},[5 95]);
+        % pct_within = sum(Ysur{i_reg} > prctiles(1) & Ysur{i_reg} < prctiles(2),'omitnan')./length(Ysur{i_reg})*100;
+        % text(0.98,0.15,sprintf('%0.1f%%\n%0.2f%s',pct_within,mdl.RMSE,units),'units','normalized','horizontalAlignment','right','fontsize',14)
+        text(0.05,0.83,sprintf('%0.1f%%',pct_within),'units','normalized','horizontalAlignment','left','fontsize',14)
+        text(0.05,0.73,sprintf('%0.2f %s',mdl.RMSE,units),'units','normalized','horizontalAlignment','left','fontsize',14)
     end
     hl = refline(1,0); hl.LineStyle='--'; hl.Color='r';
-    box on; grid on;
+    box on; %grid on;
 
-    % mdl = fitlm(Yind_ohc{i_reg},Yvld_ohc{i_reg});
     text(0.05,0.95,sprintf('(%s) %s (n=%d)',letters{i_reg},regions{i_reg},ok_runs(i_reg)),'units','normalized','fontsize',14)
     % text(0.98,0.07,sprintf('$\\epsilon_{LOO}=%0.2f$',sur_model_ohc{i_reg}.Error.LOO),'interpreter','latex','units','normalized','horizontalAlignment','right','fontsize',14)
     % set(gca,'fontsize',14,'XTickLabel',[],'YTickLabel',[])
