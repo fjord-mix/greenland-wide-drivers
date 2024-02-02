e218ec16af4e48a6b7ecfc113ca5a715be0fb951
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-02-02 13:19:47 +0200

    now returns a timetable of the time series for computing seasonal cycles

--- plotting/plot_ensemble_dt_ds.m
+++ plotting/plot_ensemble_dt_ds.m
@@ -1,5 +1,6 @@
-function hf = plot_ensemble_dt_ds(ensemble,time_axis_plt,regions_lbl)
+function [hf,tt_ensemble] = plot_ensemble_dt_ds(ensemble,time_axis_plt,regions_lbl)
 
+time_axis_plt = time_axis_plt';
 n_regions=size(ensemble,2);
 n_runs=size(ensemble,1);
 region_handles = [];
@@ -7,7 +8,7 @@ region_line_color = lines(n_regions);
 
 avg_silldepth   = NaN([1,size(ensemble,2)]);
 avg_activedepth = NaN([1,size(ensemble,2)]);
-
+tt_ensemble     = cell([1,n_regions]);
 hf = figure('Name','time series model outputs','Position',[20 20 1200 800]);
 hold on; box on
 for i_reg=1:n_regions
@@ -46,56 +47,61 @@ for i_reg=1:n_regions
         avg_silldepth(i_reg)   = mean(zsill_fjd,'omitnan');
     end
     subplot(2,2,1); hold on; box on;
-    mean_ln = mean(bootstrp(1e3,@(x)[mean(x,1,'omitnan')],ohc_reg));
+    mean_ln_temp = mean(bootstrp(1e3,@(x)[mean(x,1,'omitnan')],ohc_reg));
     std_ln  = bootci(1e3,@(x)[mean(x,1,'omitnan')],ohc_reg);
     upper_bnd = std_ln(2,:);
     lower_bnd = std_ln(1,:);
     x2 = [time_axis_plt, fliplr(time_axis_plt)];
     inBetween = [lower_bnd, fliplr(upper_bnd)];
     fill(x2, inBetween, region_line_color(i_reg,:),'edgecolor','none','facealpha',0.3);
-    plot(time_axis_plt,mean_ln,'Color',region_line_color(i_reg,:),'linewidth',1.5); 
+    plot(time_axis_plt,mean_ln_temp,'Color',region_line_color(i_reg,:),'linewidth',1.5); 
     
 
     subplot(2,2,2); hold on; box on;
-    mean_ln = mean(bootstrp(1e3,@(x)[mean(x,1,'omitnan')],osc_reg));
+    mean_ln_salt = mean(bootstrp(1e3,@(x)[mean(x,1,'omitnan')],osc_reg));
     std_ln  = bootci(1e3,@(x)[mean(x,1,'omitnan')],osc_reg);
     upper_bnd = std_ln(2,:);
     lower_bnd = std_ln(1,:);
     x2 = [time_axis_plt, fliplr(time_axis_plt)];
     inBetween = [lower_bnd, fliplr(upper_bnd)];
     fill(x2, inBetween, region_line_color(i_reg,:),'edgecolor','none','facealpha',0.3);
-    hp = plot(time_axis_plt,mean_ln,'Color',region_line_color(i_reg,:),'linewidth',1.5); 
+    hp = plot(time_axis_plt,mean_ln_salt,'Color',region_line_color(i_reg,:),'linewidth',1.5); 
     
     region_handles=[region_handles hp];
 
     subplot(2,2,3); hold on; box on;
-    mean_ln = mean(bootstrp(1e3,@(x)[mean(x,1,'omitnan')],ohc_fjd));
+    mean_ln_tf = mean(bootstrp(1e3,@(x)[mean(x,1,'omitnan')],ohc_fjd));
     std_ln  = bootci(1e3,@(x)[mean(x,1,'omitnan')],ohc_fjd);
     upper_bnd = std_ln(2,:);
     lower_bnd = std_ln(1,:);
     x2 = [time_axis_plt, fliplr(time_axis_plt)];
     inBetween = [lower_bnd, fliplr(upper_bnd)];
     % fill(x2, inBetween, region_line_color(i_reg,:),'edgecolor','none','facealpha',0.3);
-    hp_fjd = plot(time_axis_plt,mean_ln,'Color','k','linewidth',1.); 
-    plot(time_axis_plt,mean_ln,'Color',region_line_color(i_reg,:),'linewidth',1.); 
+    hp_fjd = plot(time_axis_plt,mean_ln_tf,'Color','k','linewidth',1.); 
+    plot(time_axis_plt,mean_ln_tf,'Color',region_line_color(i_reg,:),'linewidth',1.); 
 
-    mean_ln = mean(bootstrp(1e3,@(x)[mean(x,1,'omitnan')],ohc_shf));
-    hp_shf = plot(time_axis_plt,mean_ln,'Color','k','linewidth',1,'linestyle','--'); 
-    plot(time_axis_plt,mean_ln,'Color',region_line_color(i_reg,:),'linewidth',1,'linestyle','--'); 
+    mean_ln_ts = mean(bootstrp(1e3,@(x)[mean(x,1,'omitnan')],ohc_shf));
+    hp_shf = plot(time_axis_plt,mean_ln_ts,'Color','k','linewidth',1,'linestyle','--'); 
+    plot(time_axis_plt,mean_ln_ts,'Color',region_line_color(i_reg,:),'linewidth',1,'linestyle','--'); 
     
 
     subplot(2,2,4); hold on; box on;
-    mean_ln = mean(bootstrp(1e3,@(x)[mean(x,1,'omitnan')],osc_fjd));
+    mean_ln_sf = mean(bootstrp(1e3,@(x)[mean(x,1,'omitnan')],osc_fjd));
     std_ln  = bootci(1e3,@(x)[mean(x,1,'omitnan')],osc_fjd);
     upper_bnd = std_ln(2,:);
     lower_bnd = std_ln(1,:);
     x2 = [time_axis_plt, fliplr(time_axis_plt)];
     inBetween = [lower_bnd, fliplr(upper_bnd)];
     % fill(x2, inBetween, region_line_color(i_reg,:),'edgecolor','none','facealpha',0.3);
-    hp = plot(time_axis_plt,mean_ln,'Color',region_line_color(i_reg,:),'linewidth',1); 
+    hp = plot(time_axis_plt,mean_ln_sf,'Color',region_line_color(i_reg,:),'linewidth',1); 
 
-    mean_ln = mean(bootstrp(1e3,@(x)[mean(x,1,'omitnan')],osc_shf));
-    plot(time_axis_plt,mean_ln,'Color',region_line_color(i_reg,:),'linewidth',1,'linestyle','--'); 
+    mean_ln_ss = mean(bootstrp(1e3,@(x)[mean(x,1,'omitnan')],osc_shf));
+    plot(time_axis_plt,mean_ln_ss,'Color',region_line_color(i_reg,:),'linewidth',1,'linestyle','--'); 
+
+    % getting a time table with all T and S time series
+    tt_ensemble{i_reg} = timetable(time_axis_plt',mean_ln_temp',mean_ln_salt',mean_ln_tf',mean_ln_ts',mean_ln_sf',mean_ln_ss',...
+        'VariableNames',{'dT','dS','Tf','Ts','Sf','Ss'});
+
 end
 subplot(2,2,1)
 text(0.03,1.07,'(a)','fontsize',14,'units','normalized')
@@ -117,6 +123,5 @@ subplot(2,2,4)
 text(0.03,1.07,'(d)','fontsize',14,'units','normalized')
 xlabel('Time'); ylabel('Salinity');
 set(gca,'fontsize',14)
-
 
 end
