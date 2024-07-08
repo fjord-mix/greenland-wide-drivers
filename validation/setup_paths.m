b54a957ecd6c9d16c8ca86218ae895dbe1d148a7
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-07-08 14:35:00 +0100

    changed output paths to reflect current project

--- validation/setup_paths.m
+++ validation/setup_paths.m
@@ -7,6 +7,6 @@ addpath(genpath('./'))
 
 inputs_path = [data_path,'/greenland/Cowton2023GRL/'];                % where the input data is stored
 outs_path   = [data_path,'/greenland/FjordMIX/boxmodel/cowton2023/']; % where the model output files will be saved
-figs_path   = [project_path,'/figs/cowton2023/'];                     % where the figures and animations will be saved
+figs_path   = [project_path,'/figs/1_NW_fjords/'];                     % where the figures and animations will be saved
 
 fun = @(s) all(structfun(@isempty,s)); % tiny function to get rid of empty entries in array
