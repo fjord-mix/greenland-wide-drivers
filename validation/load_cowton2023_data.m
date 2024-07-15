003c228a453477ecca0abd363a009495a9fd8b8b
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-07-15 12:31:54 +0100

    gets MITgcm profile close to the ice front as opposed to fjord average

--- validation/load_cowton2023_data.m
+++ validation/load_cowton2023_data.m
@@ -1,8 +1,8 @@
-% modelled subglacial discharge
+%% modelled subglacial discharge
 qsg_glaciers = load([inputs_path,'/runoff/runoff.txt']);
 time_glaciers = datetime(load([inputs_path,'runoff/runoff_tvec.txt']));
 
-% CTD profiles
+%% CTD profiles
 ctd_data   = load([inputs_path,'/fjords/NW_fjord_TSz']);
 meta_table = readtable([inputs_path,'/fjords/Fjords_table.xlsx'],'Range','A3:I17'); % I had to unmerge some sells from the original table, otherwise Matlab would not get the header names (why were they merged in the first place?!)
 
@@ -25,12 +25,20 @@ for i=1:length(fjords_files_list)
     y_mitgcm=1:dy:size(mitgcm_out.T,2)*dy;
     z_mitgcm=1:dz:size(mitgcm_out.T,3)*dz;
     t_mitgcm=0:10:400;
-    j_fjord = 1:144;
 
     % Using zero as _fillValue is not a good practice, but should not be an issue
     mitgcm_out.T(mitgcm_out.S == 0) = NaN;
-    temp_out = squeeze(mean(mitgcm_out.T(j_fjord,:,:,:),[1,2],'omitnan'));
     mitgcm_out.S(mitgcm_out.S == 0) = NaN;
+
+    % find the ice front, which is the last NaN value in the centre line
+    land_ocn_transect = isnan(mitgcm_out.S(:,ceil(size(mitgcm_out.S,2)/2),1,1));
+    j_cf = find(land_ocn_transect==1,1,'last');
+    
+    cf_dist      = 16e3;                  % cf_dist is 16km because it's the furthest away any profile is from the ice front
+    j_downstream = j_cf+ceil(cf_dist/dy); % j_downstream is "cf_dist" km downstream from the ice front
+    j_fjord      = j_cf+1:j_downstream;   % j_fjord is then j_cf:j_downstream
+
+    temp_out = squeeze(mean(mitgcm_out.T(j_fjord,:,:,:),[1,2],'omitnan'));
     salt_out = squeeze(mean(mitgcm_out.S(j_fjord,:,:,:),[1,2],'omitnan'));
 
     mitgcm(i).id = fjord_ids_mitgcm{i};
@@ -49,6 +57,6 @@ for i=1:length(fjords_files_list)
     mitgcm(i).Sinter = mean(salt_out(5:25,:),1,'omitnan');
     mitgcm(i).Slower = mean(salt_out(25:50,:),1,'omitnan');
 
-    clear i_fjord j_fjord temp_out icebergs_out t_mitgcm z_mitgcm y_mitgcm x_mitgcm dx dy dz% bit of tidying up
+    clear i_fjord j_fjord cf_dist j_cf j_downstream temp_out icebergs_out t_mitgcm z_mitgcm y_mitgcm x_mitgcm dx dy dz% bit of tidying up
 end
 disp('MITgcm data read.')
