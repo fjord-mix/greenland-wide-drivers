95f870a7afaa6f014cebfb0cfbd125fb914d14ab
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-09-10 12:56:15 +0100

    bundles as parameters in one panel (not recommended, too busy)

---
+++ validation/grl_fjords/plot_sensitivity_profiles_v4.m
@@ -0,0 +1,219 @@
+function [hf_t,hf_s] = plot_sensitivity_profiles_v4(X,ensemble,res_box,res_obs,param_names,i_day,plt_salt,figs_path,i_yr,which_fjords)
+
+if nargin < 6, i_day=1; end
+if nargin < 7, plt_salt=0; end
+if nargin > 9
+    n_fjords = length(which_fjords);
+else
+    n_fjords = size(ensemble,1);
+end
+if size(X,2) ~=length(param_names), error('input parameter matrix must me [n_runs,n_params], and param_names must have entries for each param'); end
+n_runs = size(X,1);
+
+
+%% Finding the low/mid/high ranges for the different parameters
+key_param_bnd = {'low ','mid ','high '};
+ls_bnds = {':','-.','-'};
+
+n_std      = 1; % how many standard deviations away from the mean we want our central interval to span
+n_params   = size(X,2);
+param_bnds = NaN([4,n_params]);
+for i_param=1:n_params
+    std_param = std(X(:,i_param));
+    avg_param = mean(X(:,i_param));
+    param_bnds(:,i_param) = [min(X(:,i_param)),avg_param-n_std.*std_param,avg_param+n_std.*std_param,max(X(:,i_param))]';
+end
+
+% loops over all ensemble entries
+struct_fields = param_names;
+struct_fields{2,1} = cell(size(ensemble));
+mask_bnds = struct(struct_fields{:});
+for i_fjord=1:size(ensemble,1)
+    for i_run=1:size(ensemble,2)
+        % for each parameter, assigns a mask of {1|2|3} if the value is {low|mid|high} according to the defined param_bnds
+        if ~isempty(ensemble(i_fjord,i_run).p)
+            for i_param=1:n_params
+                if ensemble(i_fjord,i_run).p.(param_names{i_param}) > param_bnds(1,i_param) && ensemble(i_fjord,i_run).p.(param_names{i_param}) <= param_bnds(2,i_param)
+                    mask_bnds(i_fjord,i_run).(param_names{i_param}) = 1;
+                elseif ensemble(i_fjord,i_run).p.(param_names{i_param}) > param_bnds(2,i_param) && ensemble(i_fjord,i_run).p.(param_names{i_param}) <= param_bnds(3,i_param)
+                    mask_bnds(i_fjord,i_run).(param_names{i_param}) = 2;
+                elseif ensemble(i_fjord,i_run).p.(param_names{i_param}) > param_bnds(3,i_param) && ensemble(i_fjord,i_run).p.(param_names{i_param}) <= param_bnds(4,i_param)
+                    mask_bnds(i_fjord,i_run).(param_names{i_param}) = 3;
+                end
+            end
+        end
+    end
+end
+
+%% Preparing the figures
+handle_fjords = [];
+lcolor = lines(length(param_names));
+lbl_fjords = cell([1,length(param_names)]);
+
+
+hf_t = figure('Name','Temperature sensitivity','Position',[40 40 800 600]);
+% ht_t = tiledlayout(n_fjords,length(param_names));
+ht_t = tiledlayout('flow');
+
+if plt_salt
+    hf_s = figure('Name','Salinity sensitivity','Position',[40 40 900 250*length(param_names)]);
+    % ht_s = tiledlayout(n_fjords,length(param_names));
+    ht_s = tiledlayout('flow');
+end
+for i_fjord=1:size(ensemble,1)
+    if nargin > 9
+        for i_tgt_fjords=1:n_fjords
+            if strcmp(which_fjords{i_tgt_fjords},res_box(i_fjord).id) == 1
+                plot_fjord=1;
+                break
+            else
+                plot_fjord=0;
+            end
+        end
+        if ~plot_fjord, continue; end
+    end
+    %% Plotting temperature
+
+    figure(hf_t)
+    % plot(res_obs(i_fjord).ts,-res_obs(i_fjord).zs,'color',[0 0 0])
+    nexttile(i_fjord); hold on; box on
+    for i_param=1:length(param_names)    
+        for i_bnd=1:length(key_param_bnd)
+            % tf_ensemble = NaN([length(ensemble(i_fjord,1).s.z),size(ensemble,2)]);
+            tf_ensemble = NaN([length(res_box(i_fjord).zf),size(ensemble,2)]);
+            % find profiles that fit into that interval for that cast
+            for i_run=1:size(ensemble,2)
+                if isempty(ensemble(i_fjord,i_run).s), continue; end % we skip any empty entries
+                if mask_bnds(i_fjord,i_run).(param_names{i_param}) == i_bnd
+                    tf_ensemble(:,i_run) = ensemble(i_fjord,i_run).s.Tfinal(:,i_day);
+                end
+                Hsill = ensemble(i_fjord,i_run).p.Hsill;
+                Hgl   = ensemble(i_fjord,i_run).p.Hgl;
+                H     = ensemble(i_fjord,i_run).p.H;
+                has_sill = ensemble(i_fjord,i_run).p.sill;
+            end
+            
+            % take mean and min/max for that subset
+            % depths = -ensemble(i_fjord,1).s.z;
+            depths = -res_box(i_fjord).zf;
+            tfmean = mean(tf_ensemble,2,'omitnan');
+            % tfmin  = min(tf_ensemble,[],2,'omitnan');
+            % tfmax  = max(tf_ensemble,[],2,'omitnan');
+    
+            % plot
+            % y2 = [depths; flip(depths)];
+            % inBetween = [tfmin; flip(tfmax)];
+            % hfjd = patch(inBetween, y2, lcolor(i_param,:),'edgecolor',lcolor(i_param,:),'facealpha',0.1);
+            % hfjd = patch(inBetween, y2, lcolor(i_param,:),'edgecolor','none','facealpha',0.1);
+            plot(tfmean,depths,'linewidth',1.5,'color',lcolor(i_param,:),'linestyle',ls_bnds{i_bnd});
+            if i_bnd==length(key_param_bnd)
+                hp = plot(tfmean,depths,'linewidth',1.5,'color',lcolor(i_param,:),'linestyle',ls_bnds{i_bnd});
+            end
+        end
+        % add depictions of GL and sill depths
+        base_gl_and_sill_t = 0;
+        scatter(base_gl_and_sill_t,-Hgl,40,'v','filled','MarkerFaceColor',[0 0 0])
+        if has_sill
+            plot([base_gl_and_sill_t base_gl_and_sill_t],[-H -Hsill],'-','linewidth',2,'color',[0 0 0])
+        end
+        set(gca,'fontsize',14)
+        % xlim([-2 5])
+        ylim([-H 0])
+        if i_fjord==1
+            handle_fjords = [handle_fjords hp];
+            lbl_fjords{i_fjord} = param_names{i_param};%res_box(i_fjord).id;
+        end
+        if i_param==1
+            % text(0.02,0.05,sprintf("%s = [%.1e,%.1e]",param_names{i_param},param_bnds(i_bnd,i_param),param_bnds(i_bnd+1,i_param)),'units','normalized','fontsize',14)
+            text(0.02,1.075,sprintf("(%s) %s",res_box(i_fjord).id,res_box(i_fjord).name),'units','normalized','fontsize',12)
+        end
+    end
+    if i_fjord==1
+        legend(gca,handle_fjords,param_names,'fontsize',12,'Location','southeast');
+        % hl{i_bnd}.NumColumns=2;
+    end
+
+    %% Plotting salinity
+    if plt_salt
+        figure(hf_s)
+        nexttile(i_fjord); hold on; box on
+        
+        for i_param=1:length(param_names)    
+            for i_bnd=1:length(key_param_bnd)
+                sf_ensemble = NaN([length(ensemble(i_fjord,1).s.z),size(ensemble,2)]);
+                % find profiles that fit into that interval for that cast
+                for i_run=1:size(ensemble,2)
+                    if isempty(ensemble(i_fjord,i_run).s), continue; end % we skip any empty entries
+                    if mask_bnds(i_fjord,i_run).(param_names{i_param}) == i_bnd
+                        sf_ensemble(:,i_run) = ensemble(i_fjord,i_run).s.Sfinal(:,i_day);
+                    end
+                end
+                
+                % take mean and min/max for that subset
+                depths = -res_box(i_fjord).zf;
+                sfmean = mean(sf_ensemble,2,'omitnan');
+                % sfmin  = min(sf_ensemble,[],2,'omitnan');
+                % sfmax  = max(sf_ensemble,[],2,'omitnan');
+        
+                % plot
+                y2 = [depths; flip(depths)];
+                % inBetween = [sfmin; flip(sfmax)];
+                % % hfjd = patch(inBetween, y2, lcolor(i_param,:),'edgecolor',lcolor(i_param,:),'facealpha',0.1);
+                % patch(inBetween, y2, lcolor(i_param,:),'edgecolor','none','facealpha',0.1);
+                plot(sfmean,depths,'linewidth',1.5,'color',lcolor(i_param,:),'linestyle',ls_bnds{i_bnd});
+                if i_bnd==length(key_param_bnd)
+                    hp = plot(sfmean,depths,'linewidth',1.5,'color',lcolor(i_param,:),'linestyle',ls_bnds{i_bnd});
+                end
+            end
+            % add depictions of GL and sill depths
+            base_gl_and_sill_s = 33;
+            scatter(base_gl_and_sill_s,-Hgl,40,'v','filled','MarkerFaceColor',[0 0 0])
+            if has_sill
+                plot([base_gl_and_sill_s base_gl_and_sill_s],[-H -Hsill],'-','linewidth',2,'color',[0 0 0])
+            end
+            set(gca,'fontsize',14)
+            xlim([30.5 35])
+            ylim([-H 0])
+            % if i_fjord==1
+            %     handle_fjords = [handle_fjords hfjd];
+            %     lbl_fjords{i_fjord} = param_names{i_param};%res_box(i_fjord).id;
+            % end
+            if i_param==1
+                % text(0.02,0.05,sprintf("%s = [%.1e,%.1e]",param_names{i_param},param_bnds(i_bnd,i_param),param_bnds(i_bnd+1,i_param)),'units','normalized','fontsize',14)
+                text(0.02,0.09,sprintf("(%s) %s",res_box(i_fjord).id,res_box(i_fjord).name),'units','normalized','fontsize',12)
+            end
+        end
+        if i_fjord==1
+            legend(gca,handle_fjords,strcat(key_param_bnd{i_bnd},param_names),'fontsize',10,'Location','West');
+            % hl{i_bnd}.NumColumns=2;
+        end
+    end
+
+    
+end
+figure(hf_t)
+% nexttile; axis off
+% hl.Layout.Tile='southeast';
+xlabel(ht_t,'Temperature (^oC)','fontsize',14);
+ylabel(ht_t,'Depth (m)','fontsize',14);
+ht_t.TileSpacing='compact';
+ht_t.Padding='compact';
+
+if plt_salt
+    figure(hf_s)
+    xlabel(ht_s,'Salinity','fontsize',14);
+    ylabel(ht_s,'Depth (m)','fontsize',14);
+    ht_s.TileSpacing='compact';
+    ht_s.Padding='compact';
+end
+
+if nargin > 8 && ~isempty(figs_path)
+    figure(hf_t)
+    exportgraphics(gcf,[figs_path,'sensitivity_profiles_temp_simple_',num2str(2015+i_yr),'_n',num2str(n_runs),'.png'],'Resolution',300)
+
+    if plt_salt
+        figure(hf_s)
+        exportgraphics(gcf,[figs_path,'sensitivity_profiles_salt_simple_',num2str(2015+i_yr),'_n',num2str(n_runs),'.png'],'Resolution',300)
+    end
+end
+end
