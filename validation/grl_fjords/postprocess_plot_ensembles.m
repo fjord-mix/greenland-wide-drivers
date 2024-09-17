2f5164d383f6fa9c77f4975427d04ce271edbdc2
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-09-17 17:11:01 +0100

    adjusted which figures we want to plot and save

--- validation/grl_fjords/postprocess_plot_ensembles.m
+++ validation/grl_fjords/postprocess_plot_ensembles.m
@@ -26,16 +26,19 @@ for i_yr_load=1:n_years_to_plot
     ensemble_yr{i_yr_load}    = ensemble;
     fjord_model_yr{i_yr_load} = fjord_model;
     fprintf('Postprocessing %d done.\n',which_year_load)
-    % plot_ensemble_profiles(fjord_model,ensemble_yr{i_yr_load},res_box_yr{i_yr_load},res_obs_yr{i_yr_load},n_runs,param_names,tgt_days(2),name_days,2);
-    plot_ensemble_profiles(fjord_model,ensemble_yr{i_yr_load},res_box_yr{i_yr_load},res_obs_yr{i_yr_load},n_runs,param_names,tgt_days(2),[],2,[],0,0,0,[],1);
+    % plot_ensemble_profiles(fjord_model,ensemble_yr{i_yr_load},res_box_yr{i_yr_load},res_obs_yr{i_yr_load},n_runs,param_names,tgt_days(2),[],2);
+    % plot_ensemble_profiles(fjord_model,ensemble_yr{i_yr_load},res_box_yr{i_yr_load},res_obs_yr{i_yr_load},n_runs,param_names,tgt_days(2),[],2,[],0,0,0,[],1);
     % exportgraphics(gcf,[figs_path,'profiles_GRL_temp_',num2str(which_year_load),'_n',num2str(n_runs),'.png'],'Resolution',300)
     % exportgraphics(gcf,[figs_path,'rmse_temp_rpm_shelf_GRL_',num2str(which_year_load),'_n',num2str(n_runs),'.png'],'Resolution',300)
-    plot_sensitivity_profiles_v3(X,ensemble_yr{i_yr_load},res_box_yr{i_yr_load},res_obs_yr{i_yr_load},param_names,2,0,[],i_yr_load, which_fj_sens{i_yr_load});
-    exportgraphics(gcf,[figs_path,'sensitivity_profiles_temp_',num2str(which_year_load),'_n',num2str(n_runs),'.png'],'Resolution',300)
+    % plot_sensitivity_profiles_v3(X,ensemble_yr{i_yr_load},res_box_yr{i_yr_load},res_obs_yr{i_yr_load},param_names,2,0,[],i_yr_load, which_fj_sens{i_yr_load});
+    % exportgraphics(gcf,[figs_path,'supp/sensitivity_profiles_temp_',num2str(which_year_load),'_n',num2str(n_runs),'.png'],'Resolution',300)
+    % plot_sensitivity_scatter(X,ensemble_yr{i_yr_load},res_box_yr{i_yr_load},param_names,2,1,which_fj_sens{i_yr_load});
+    plot_sensitivity_scatter(X,ensemble_yr{i_yr_load},res_box_yr{i_yr_load},param_names,2,0,which_fj_sens{i_yr_load});
+    exportgraphics(gcf,[figs_path,'supp/sensitivity_scatter_temp_',num2str(which_year_load),'_n',num2str(n_runs),'.png'],'Resolution',300)
     close all
 end
 plot_best_params_time(fjord_IDs,fjord_model_yr,ensemble_yr,res_box_yr,param_names,range_params,2);
-exportgraphics(gcf,[figs_path,'best_params_GRL_scatter',num2str(which_year_load),'_n',num2str(n_runs),'.png'],'Resolution',300)
+exportgraphics(gcf,[figs_path,'supp/best_params_GRL_scatter',num2str(which_year_load),'_n',num2str(n_runs),'.png'],'Resolution',300)
 plot_best_params_rmse(fjord_IDs,fjord_model_yr,ensemble_yr,res_box_yr,param_names,range_params,2);
 exportgraphics(gcf,[figs_path,'best_params_GRL_rmse',num2str(which_year_load),'_n',num2str(n_runs),'.png'],'Resolution',300)
 % plot_best_params_dist(fjord_IDs,fjord_model_yr,ensemble_yr,res_box_yr,param_names,range_params,2);
