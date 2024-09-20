392f65604bc614b75ce12730a524a2f367d81c75
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-09-20 13:01:18 +0100

    histogram plot to show depth at which FW is exported

---
+++ validation/grl_fjords/plot_hist_zfw_export.m
@@ -0,0 +1,29 @@
+function [hf_zfw] = plot_hist_zfw_export(ensemble_yr,i_tgt_day)
+
+n_years = length(ensemble_yr);
+lcolor=lines(n_years);
+h_hist = [];
+lbl_hist = {};
+
+hf_zfw = figure; hold on; box on; grid on;
+for i_year=n_years:-1:1
+    ensemble = ensemble_yr{i_year};
+    zfw = NaN(size(ensemble));
+    for i_fjord=1:size(ensemble,1)
+        for i_run=1:size(ensemble,2)
+            if ~isempty(ensemble(i_fjord,i_run).s)
+                zfw(i_fjord,i_run) = ensemble(i_fjord,i_run).s.z_max_export(i_tgt_day);
+            end
+        end
+    end
+    h1 = histogram(zfw(:),ensemble(1,1).p.N,'FaceColor',lcolor(i_year,:),'orientation','horizontal','FaceAlpha',0.5);
+    h_sit = [h_hist h1];
+    lbl_hist{end+1} = num2str(2015+i_year);
+end
+
+legend(h_hist,lbl_hist,'location','best')
+ylabel('Depth of maximum FW export (m)')
+xlabel('Count')
+set(gca,'fontsize',16)
+
+end
