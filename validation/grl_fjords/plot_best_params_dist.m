f4bcd2a724400edfcbff0d0207a93c854cad469c
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-09-20 12:56:00 +0100

    filtered by RMSE and added a 'total all-years count' histogram

--- validation/grl_fjords/plot_best_params_dist.m
+++ validation/grl_fjords/plot_best_params_dist.m
@@ -7,7 +7,9 @@ fsize    = 14;
 n_years  = length(fjord_model_yr);
 n_params = length(param_names);
 lcolor   = cmocean('thermal',n_years);
-param_entries = cell(1,n_years);
+
+rmse_tf_threshold = 0.5;
+rmse_sf_threshold = 0.05;
 
 fjord_names = cell([1,length(fjord_IDs)]);
 for i=1:length(fjord_names), fjord_names{i} = fjord_IDs(i); end
@@ -15,7 +17,7 @@ for i=1:length(fjord_names), fjord_names{i} = fjord_IDs(i); end
 hf = figure('Name','Best parameters','Position',[40 40 1200 1000]);
 tiledlayout('flow');
 
-
+param_entries_all = cell([1,n_params]);
 h_yr = [];
 lbl_years = cell([n_years,1]);
 for i_yr=1:n_years
@@ -33,10 +35,16 @@ for i_yr=1:n_years
     
     for i_fjord=1:n_fjords
     
+        % Filter out runs that have a high RMSE, i.e., we want to focus on the fjords we can simulate well
+        rmse_tf_filtered = res_box(i_fjord).rmse_tf;
+        rmse_sf_filtered = res_box(i_fjord).rmse_sf;
+        rmse_tf_filtered(rmse_tf_filtered>rmse_tf_threshold) = NaN;
+        rmse_sf_filtered(rmse_sf_filtered>rmse_sf_threshold) = NaN;
+
         % find run with the smallest RMSE
-        rmse_both = w_rmse_t.*res_box(i_fjord).rmse_tf + (1-w_rmse_t).*res_box(i_fjord).rmse_sf;
-        [~,inds_best_tf] = min(squeeze(res_box(i_fjord).rmse_tf(:,i_tgt_day)),[],'all','omitnan');
-        [~,inds_best_sf] = min(squeeze(res_box(i_fjord).rmse_sf(:,i_tgt_day)),[],'all','omitnan');
+        rmse_both = w_rmse_t.*rmse_tf_filtered + (1-w_rmse_t).*rmse_sf_filtered;
+        [~,inds_best_tf] = min(squeeze(rmse_tf_filtered(:,i_tgt_day)),[],'all','omitnan');
+        [~,inds_best_sf] = min(squeeze(rmse_tf_filtered(:,i_tgt_day)),[],'all','omitnan');
         [~,inds_best2]    = min(squeeze(rmse_both(:,i_tgt_day)),[],'all','omitnan');
         
         for i_param=1:n_params
@@ -55,13 +63,16 @@ for i_yr=1:n_years
         nexttile(i_param); box on; hold on; 
         x_bnds = range_params{i_param};
         x_val = linspace(x_bnds(1),x_bnds(2),100);
+        param_entry_filtered = param_entry(:,i_param);
+        param_entries_all{i_param} = [param_entries_all{i_param}; param_entry_filtered];
+        
         if strcmp(which_dist,'hist') == 1
-            h1 = histogram(param_entry(:,i_param),20,'FaceColor',lcolor(i_yr,:),'FaceAlpha',0.5); % if there are no distributions, we plot a histogram
+            h1 = histogram(param_entry_filtered,20,'FaceColor',lcolor(i_yr,:),'FaceAlpha',0.5); % if there are no distributions, we plot a histogram
         else
             if iscell(which_dist)
-                kern_marginal= fitdist(param_entry(:,i_param),which_dist{i_param}); % in case we want different distributions for each variable
+                kern_marginal= fitdist(param_entry_filtered,which_dist{i_param}); % in case we want different distributions for each variable
             else
-                kern_marginal= fitdist(param_entry(:,i_param),which_dist); % otherwise it is all the same
+                kern_marginal= fitdist(param_entry_filtered,which_dist); % otherwise it is all the same
             end
             h1 = plot(x_val,pdf(kern_marginal,x_val),'linewidth',2,'color',lcolor(i_yr,:));
         end
@@ -77,6 +88,7 @@ for i_yr=1:n_years
             % xlim(range_params{i_param})
             % vline(range_params{i_param},'--','color',[0.25 0.25 0.25])
             xlim(range_params{i_param});
+            histogram(param_entries_all{i_param},20,'FaceColor',[0.7 0.7 0.7],'FaceAlpha',0.75); % if there are no distributions, we plot a histogram
         end
     end
     h_yr = [h_yr h1];
