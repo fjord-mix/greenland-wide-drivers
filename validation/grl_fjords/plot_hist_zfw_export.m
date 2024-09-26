3c9535ee394bb4d598aa0c91619c30c5769a4bb2
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-09-26 10:55:00 +0100

    two separate figures for depth and time of FW export

--- validation/grl_fjords/plot_hist_zfw_export.m
+++ validation/grl_fjords/plot_hist_zfw_export.m
@@ -1,4 +1,4 @@
-function [hf_zfw] = plot_hist_zfw_export(ensemble_yr,i_tgt_day)
+function [hf_zfw,hf_tfw] = plot_hist_zfw_export(ensemble_yr,i_tgt_day)
 
 n_years = length(ensemble_yr);
 lcolor=lines(n_years);
@@ -7,14 +7,20 @@ lbl_hist = {};
 zfw_stacked = [];
 znb_stacked = [];
 tfw_stacked = [];
+t_qsg_stacked = [];
 
-hf_zfw = figure('Position',[50 50, 1200 400]); 
+hf_zfw = figure('Position',[50 50, 900 400]); 
 ht_zfw = tiledlayout('horizontal');
+
+hf_tfw = figure('Position',[50 50, 600 800]); 
+ht_tfw = tiledlayout('vertical');
+
 for i_year=n_years:-1:1
     ensemble = ensemble_yr{i_year};
     zfw = NaN(size(ensemble));
     znb = NaN(size(ensemble));
     t_fw_max = NaN(size(ensemble));
+    t_qsg_max = NaN([size(ensemble,1),1]);
     for i_fjord=1:size(ensemble,1)
         for i_run=1:size(ensemble,2)
             if ~isempty(ensemble(i_fjord,i_run).s)
@@ -30,7 +36,9 @@ for i_year=n_years:-1:1
                 [~,i_fz] = min(ensemble(i_fjord,i_run).s.fw_export_t(end-365:end));
                 % zfw(i_fjord,i_run) = ensemble(i_fjord,i_run).s.z_max_export_t(end-365+i_fz);
                 % znb(i_fjord,i_run) = ensemble(i_fjord,i_run).s.znb_t(end-365+i_fz);
+
                 t_fw_max(i_fjord,i_run) = i_fz;
+                [~,t_qsg_max(i_fjord)] = max(ensemble(i_fjord,i_run).s.Qsg(end-365:end));
 
                 zfw_stacked = [zfw_stacked; zfw(:)];
                 znb_stacked = [znb_stacked; znb(:)];
@@ -38,26 +46,36 @@ for i_year=n_years:-1:1
             end
         end
     end
+    t_qsg_stacked = [t_qsg_stacked; t_qsg_max(:)];
+
+    figure(hf_zfw)
     nexttile(1); hold on; box on; grid on;
-    title('Neutral buoyancy')
+    title('(a) Neutral buoyancy')
     histogram(znb(:),ensemble(1,1).p.N,'Normalization','probability','FaceColor',lcolor(i_year,:),'orientation','horizontal','FaceAlpha',0.5);
     set(gca, 'xdir', 'reverse','fontsize',16);
     ylim([-450 0])
 
     nexttile(2); hold on; box on; grid on;
-    title('Maximum FW export')
+    title('(b) Maximum FW export')
     h1 = histogram(zfw(:),ensemble(1,1).p.N,'Normalization','probability','FaceColor',lcolor(i_year,:),'orientation','horizontal','FaceAlpha',0.5);
     set(gca,'fontsize',16)
     ylim([-450 0])
 
-    nexttile(3); hold on; box on; grid on;
-    title('Time of maximum FW export')
+    figure(hf_tfw)
+    nexttile(1); hold on; box on; grid on;
+    title('(a) Time of maximum FW export')
     histogram(t_fw_max(:),ensemble(1,1).p.N,'Normalization','probability','FaceColor',lcolor(i_year,:),'orientation','vertical','FaceAlpha',0.5);
     set(gca,'fontsize',16)
+
+    nexttile(2); hold on; box on; grid on;
+    title('(b) Time of maximum subglacial discharge')
+    histogram(t_qsg_max(:),ensemble(1,1).p.N,'Normalization','probability','FaceColor',lcolor(i_year,:),'orientation','vertical','FaceAlpha',0.5);
+    set(gca,'fontsize',16,'ydir','reverse')
 
     h_sit = [h_hist h1];
     lbl_hist{end+1} = num2str(2015+i_year);
 end
+figure(hf_zfw)
 nexttile(1); hold on;
 histogram(znb_stacked(:),ensemble(1,1).p.N,'Normalization','probability','FaceColor',[0 0 0],'orientation','horizontal','FaceAlpha',1.);
 xlabel('Probability','fontsize',16)
@@ -66,12 +84,18 @@ histogram(zfw_stacked(:),ensemble(1,1).p.N,'Normalization','probability','FaceCo
 xlabel('Probability','fontsize',16)
 legend(h_hist,lbl_hist,'location','best')
 
-nexttile(3); hold on;
-histogram(t_fw_max(:),ensemble(1,1).p.N,'Normalization','probability','FaceColor',[0 0 0],'orientation','vertical','FaceAlpha',1.);
-xlabel('Day of year'); ylabel('Probability')
+figure(hf_tfw)
+nexttile(1); hold on;
+histogram(tfw_stacked(:),ensemble(1,1).p.N,'Normalization','probability','FaceColor',[0 0 0],'orientation','vertical','FaceAlpha',1.);
+xlim([100 300])
+nexttile(2); hold on;
+histogram(t_qsg_stacked(:),ensemble(1,1).p.N,'Normalization','probability','FaceColor',[0 0 0],'orientation','vertical','FaceAlpha',1.);
 xlim([100 300])
 
 ylabel(ht_zfw,'Depth (m)','fontsize',16)
-% xlabel(ht_zfw,'Probability','fontsize',16)
+xlabel(ht_zfw,'Probability','fontsize',16)
+
+xlabel(ht_tfw,'Day of year','fontsize',16)
+ylabel(ht_tfw,'Probability','fontsize',16)
 
 end
